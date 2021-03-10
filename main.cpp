426b83f89a7256b452fc59282f09d524d2253dd6
Alvarez Romain <roalvare@student.42.fr>
2021-03-10 13:08:21 +0100

    Begin vector (assign, push_back, [])

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include "list.hpp"
+#include "vector.hpp"
 #include <iostream>
 
 int main(){
-	ft::list<int> list(3, 1);
+	ft::vector<int> list;
+	list.push_back(9);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(1);
+	list.push_back(0);
+	list.push_back(6);
+	list.push_back(7);
+	list.push_back(42);
+	list.print();
+	list.assign(3, 3);
+	list.print();
 }
---
+++ vector.hpp
@@ -0,0 +1,114 @@
+#ifndef VECTOR_H
+# define VECTOR_H
+
+#include <iostream>
+
+namespace ft{
+	template < typename T >
+	class vector
+	{
+	private:
+		typedef T					value_type;
+		typedef value_type&			reference;
+		typedef const value_type&	const_reference;
+		typedef value_type*			pointer;
+		typedef const value_type*	const_pointer;
+		typedef std::ptrdiff_t		differrence_type;
+		typedef size_t				size_type;
+
+		size_type					_size;
+		size_type					_capacity;
+		T *							_data;
+
+		size_type	get_fit_capacity(size_type target){
+			size_type	capacity = 8;
+			while (capacity < target)
+				capacity *= 2;
+			return (capacity);
+		}
+
+		void	realloc() {
+			_capacity *= 2;
+			T *	new_data = new T[_capacity]();
+			for (size_t i = 0; i < _size; i++)
+				new_data[i] = this->_data[i];
+			delete[] _data;
+			_data = new_data;
+		}
+		void	realloc(size_type target) {
+			_capacity = get_fit_capacity(target);
+			T *	new_data = new T[_capacity]();
+			for (size_t i = 0; i < _size; i++)
+				new_data[i] = this->_data[i];
+			delete[] _data;
+			_data = new_data;
+		}
+	public:
+		vector(void) : _size(0), _capacity(8), _data(new T[8]()) {}
+		~vector(void) {
+			delete[] _data;
+		}
+		vector& operator= (const vector& x) {
+			if (this != &x){
+				delete[] this->_data;
+				this->_size = x->_size;
+				this->_capacity = x->_capacity;
+				this->_data = new T[_capacity];
+				for (size_t i = 0; i < this->_capacity; i++)
+					this->_data[i] = x->_data[i];
+			}
+			return (*this);
+		}
+
+		size_type	size() const {
+			return (_size);
+		}
+
+		size_type	capacity() const {
+			return (_capacity);
+		}
+
+		bool		empty() const {
+			if(size())
+				return (false);
+			return (true);
+		}
+
+		reference operator[](size_type n) {
+			return (_data + n);
+		}
+
+		const_reference operator[](size_type n) const {
+			return (_data + n);
+		}
+
+		void assign (size_type n, const value_type& val) {
+			realloc(n);
+			_size = n;
+			for (size_t i = 0; i < n; i++)
+				_data[i] = val;
+		}
+
+		void push_back (const value_type& val) {
+			size_type	size = this->size();
+			if (size == capacity())
+				realloc();
+			_data[size] = val;
+			_size++;
+		}
+
+		// DELETE
+		void print(void) {
+			for (size_t i = 0; i < this->size(); i++)
+			{
+				std::cout << _data[i];
+				if (i == this->size() -1)
+					std::cout << std::endl;
+				else
+					std::cout << ", ";
+			}
+		}
+	};
+}
+
+#endif

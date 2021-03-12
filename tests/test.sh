13501fdcf79946765159df83eb836e2c0ebdc609
Alvarez Romain <roalvare@student.42.fr>
2021-03-12 16:34:51 +0100

    option verbose

--- tests/test.sh
+++ tests/test.sh
@@ -10,7 +10,8 @@ reset=$'\033[0;39m'
 STANDAR_OUPUT_FILE=std.out
 FT_OUPUT_FILE=ft.out
 DEBUG=0
-VERBOSE=1
+VERBOSE=0
+newline=1
 
 
 
@@ -20,12 +21,16 @@ function	print_message {
 	if [ -z "$type" ]; then
 		type=${output}
 	fi
-	echo -e "${type}$1${reset}"
+	if [ $newline -eq 0 ];then
+		echo -n -e "${type}$1${reset}"
+	else
+		echo -e "${type}$1${reset}"
+	fi
 }
 
 #			print_error(message)
 function	print_error {
-	print_message "Error : "$1 $error
+	print_message "[Error] : "$1 $error
 }
 
 #			print_warning(message)
@@ -45,24 +50,29 @@ function	unit_test {
 	test_name=$(echo "$IN" | cut -d/ -f 3 | cut -d. -f 1)
 	STD_LOG=./log/"$container_name"_"$test_name"_std
 	FT_LOG=./log/"$container_name"_"$test_name"_ft
-	print_message $test_name
-	clang++ -Werror -Wextra -Wall -std=c++98 -I ../ -I./includes $file -o std.out &>> $STD_LOG || { print_warning "complilation with standar namespace fail, skip test"; return 1; }
-	./std.out &>> $STD_LOG || { print_warning "exec with standar namespace fail, skip test"; return 1; }
-	clang++ -Werror -Wextra -Wall -std=c++98 -I ../ -I./includes $file -D NAMESPACE=ft -o ft.out &>> $FT_LOG || { print_error "compilation fail"; return 1; }
-	./ft.out &>> $FT_LOG || { print_error "exec fail"; return 1; }
+
+	if [ $VERBOSE -eq 1 ];then
+		print_message $test_name
+	else
+		newline=0
+	fi;
+	clang++ -Werror -Wextra -Wall -std=c++98 -I ../ -I./includes $file -o std.out &>> $STD_LOG || { print_warning "complilation with standar namespace fail, skip test " ; return 1; }
+	./std.out &>> $STD_LOG || { print_warning "exec with standar namespace fail, skip test "; return 1; }
+	clang++ -Werror -Wextra -Wall -std=c++98 -I ../ -I./includes $file -D NAMESPACE=ft -o ft.out &>> $FT_LOG || { print_error "compilation fail " ; return 1; }
+	./ft.out &>> $FT_LOG || { print_error "exec fail" $newline; return 1; }
 	diff $STD_LOG $FT_LOG
 	if [ $? -eq 0 ];then
-		print_success "[OK]"
+		print_success "[OK] "
 		rm -rf $FT_LOG $STD_LOG
 	else
-		print_message "[K0]" $error
+		print_message "[K0] " $error
 	fi
+	newline=1
 }
 
 #			container_test(name, n=none)
 function	container_test {
-	# if [ -n $2 ];then
-	# 	container_test vector
+	print_message $1
 	for file in ./$1/$2*
 	do
 		unit_test $file

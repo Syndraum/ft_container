fc07bb1a913076c91e5826c0bf62d0012f8c5075
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-03-14 12:22:27 +0100

    adding more is_integral

--- utils.hpp
+++ utils.hpp
@@ -15,6 +15,17 @@ namespace ft{
 
 	template <class T> struct is_integral : public false_type {};
 	template <> struct is_integral<int> : public true_type {};
+	template <> struct is_integral<unsigned int> : public true_type {};
+	template <> struct is_integral<bool> : public true_type {};
+	template <> struct is_integral<char> : public true_type {};
+	template <> struct is_integral<unsigned char> : public true_type {};
+	template <> struct is_integral<signed char> : public true_type {};
+	template <> struct is_integral<short> : public true_type {};
+	template <> struct is_integral<unsigned short> : public true_type {};
+	template <> struct is_integral<long> : public true_type {};
+	template <> struct is_integral<unsigned long> : public true_type {};
+	template <> struct is_integral<long long> : public true_type {};
+	template <> struct is_integral<unsigned long long> : public true_type {};
 
 	template<bool B, class T = void>
 		struct enable_if {};

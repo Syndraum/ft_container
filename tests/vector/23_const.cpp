e63dc29057c72f8baf28eba9b0f01bf66a8d6b43
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-03-15 15:08:35 +0100

    better test it_cont

--- tests/vector/23_const.cpp
+++ tests/vector/23_const.cpp
@@ -4,10 +4,38 @@ using namespace NAMESPACE;
 
 int main()
 {
-	vector<int> myvector(2, 100);
-	return (0);
+	vector<int> myvector(10);
+
+	for (size_t i = 0; i < myvector.size(); i++){
+		myvector[i] = i * i;
+	}
 	
 	vector<int>::const_iterator cit = myvector.begin();
 	std::cout << *cit << std::endl;
-	
+	vector<int>::const_iterator cit_copy(cit);
+	std::cout << *cit_copy << std::endl;
+
+
+	std::cout << std::boolalpha << (cit == cit_copy) << std::endl;
+	std::cout << std::boolalpha << (cit != cit_copy) << std::endl;
+
+	vector<int>::const_iterator cit2 = cit + 3;
+	std::cout << *cit2 << std::endl;
+
+	std::cout << "it + 3 : " << *(cit2 + 3) << std::endl;
+	std::cout << "2 + it : "<< *(2 + cit2) << std::endl;
+	std::cout << "it - 2 : " << *(cit2 - 2) << std::endl;
+	std::cout << "it - it_copy : " << cit2 - cit_copy << std::endl;
+
+	std::cout << std::boolalpha << (cit <= cit_copy) << std::endl;
+	std::cout << std::boolalpha << (cit >= cit_copy) << std::endl;
+	std::cout << std::boolalpha << (cit < cit_copy) << std::endl;
+	std::cout << std::boolalpha << (cit > cit_copy) << std::endl;
+
+	for (size_t i = 0; i < myvector.size(); i++)
+	{
+		std::cout << myvector[i] << std::endl;
+	}
+
+	return (0);
 }

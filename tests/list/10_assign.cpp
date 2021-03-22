f2313b8076aa0ed6be6df5b4ea105047e617c4b9
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-03-22 11:48:03 +0000

    adding modifier test

---
+++ tests/list/10_assign.cpp
@@ -0,0 +1,20 @@
+#include "test_utils.hpp"
+
+using namespace NAMESPACE;
+
+int main ()
+{
+  list<int> first;
+  list<int> second;
+
+  first.assign (7,100);                      // 7 ints with value 100
+
+  second.assign (first.begin(),first.end()); // a copy of first
+
+  int myints[]={1776,7,4};
+  first.assign (myints,myints+3);            // assigning from array
+
+  std::cout << "Size of first: " << int (first.size()) << '\n';
+  std::cout << "Size of second: " << int (second.size()) << '\n';
+  return 0;
+}
---
+++ tests/list/13_push_back.cpp
@@ -0,0 +1,17 @@
+#include "test_utils.hpp"
+
+using namespace NAMESPACE;
+
+int main ()
+{
+  list<int> mylist (2,100);         // two ints with a value of 100
+  mylist.push_front (200);
+  mylist.push_front (300);
+
+  std::cout << "mylist contains:";
+  for (list<int>::iterator it=mylist.begin(); it!=mylist.end(); ++it)
+    std::cout << ' ' << *it;
+
+  std::cout << '\n';
+  return 0;
+}

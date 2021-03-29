ef65f5274ce168af54b8fdc3f7fc6cd9b386c7f2
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-03-29 12:40:52 +0000

    adding test iterator

---
+++ tests/map/04_reverse_iterator.cpp
@@ -0,0 +1,19 @@
+#include "test_utils.hpp"
+
+using namespace NAMESPACE;
+
+int main ()
+{
+  map<char,int> mymap;
+
+  mymap['x'] = 100;
+  mymap['y'] = 200;
+  mymap['z'] = 300;
+
+  // show content:
+  map<char,int>::reverse_iterator rit;
+  for (rit=mymap.rbegin(); rit!=mymap.rend(); ++rit)
+    std::cout << rit->first << " => " << rit->second << '\n';
+
+  return 0;
+}

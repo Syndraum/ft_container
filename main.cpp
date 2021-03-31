f046d9965007def5f90d982ed6f5823d08e8d425
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-03-31 09:18:27 +0000

    add test reverse it

--- main.cpp
+++ main.cpp
@@ -19,18 +19,33 @@ int main(){
 	mymap['a'] = 3;
 	mymap['f'] = 1;
 	mymap['b'] = 0;
-	mymap.print();
+
+	ft::vector<int> mylist;
+	mylist.push_back(1);
+	mylist.push_back(42);
+	mylist.push_back(3);
+	mylist.push_back(0);
+	mylist.push_back(-6);
+
+	for (ft::vector<int>::reverse_iterator it = mylist.rbegin(); it != mylist.rend(); it++)
+		std::cout << *it << " ";
+	std::cout << '\n';
+
+	for (ft::vector<int>::const_reverse_iterator cit = mylist.rbegin(); cit != mylist.rend(); cit++)
+		std::cout << *cit << " ";
+	std::cout << '\n';
 
 	// ft::map<char, int> const const_map(mymap);
 
-	ft::map<char, int>::iterator it = mymap.begin();
-	ft::map<char, int>::btree_type *node = it.getNode();
-	std::cout << "value : " << node->value->first << "\n";
+	
+	// ft::map<char, int>::iterator it = mymap.begin();
+	// ft::map<char, int>::btree_type *node = it.getNode();
+	// std::cout << "value : " << node->value->first << "\n";
 
-	ft::map<char, int>::btree_type const *const_node = it.getNode();
-	std::cout << "value : " << const_node->value->first << "\n";
-	ft::map<char, int>::iterator cit(mymap.begin());
-	std::cout << "value : " << cit->first << "\n";
+	// ft::map<char, int>::btree_type const *const_node = it.getNode();
+	// std::cout << "value : " << const_node->value->first << "\n";
+	// ft::map<char, int>::iterator cit(mymap.begin());
+	// std::cout << "value : " << cit->first << "\n";
 
 	return 0;
 }
